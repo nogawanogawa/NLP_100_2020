1b8817e01f44e22574aa1dee9603ce3646b80e85
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-04-15 04:09:14 +0900

    [add] 37

---
+++ ch04/36.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import japanize_matplotlib
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("neko.txt.mecab", header=None, sep='\t|,', usecols=[
+                 0, 1, 2, 7], names=['surface', 'pos', 'pos1', 'base'], skiprows=4, skipfooter=1)
+
+words = df["base"]
+sr = pd.Series(words)
+counts = sr.value_counts()
+a = pd.DataFrame(counts).reset_index()
+a = a[:10]
+a = a.set_index("index")
+a.plot.bar(y=['base'], figsize=(12, 3), legend=False)
+plt.show()
