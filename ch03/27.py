8c7c6b5d30ffd49a8479733d99d706efd9404404
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-04-14 14:29:07 +0900

    [add] 27

---
+++ ch03/27.py
@@ -0,0 +1,26 @@
+import json
+import re
+
+with open('jawiki-country.json', 'r') as f:
+    l_strip = [s.strip() for s in f.readlines()]
+
+i = json.loads(l_strip[10])["text"]
+
+m1 = re.search(r'{{基礎情報 国.*', i)  # 開始
+m2 = re.search(r'(.*)\n}}\n', i[m1.end():])  # 終了
+template = i[m1.end():m2.end()+1]
+template = template.split("\n")
+
+d = {}
+for t in template:
+    m = re.match(r'\|(.+) = (.*)', t)
+    if m is not None:
+        # 差分
+        key = re.sub(r'\'\'*', "", m.group(1))
+        value = re.sub(r'\'\'*', "", m.group(2))
+        key = re.sub(r"\[\[(?:[^:\]]+?\|)?([^:]+?)\]\]", r"\1", key)
+        value = re.sub(r"\[\[(?:[^:\]]+?\|)?([^:]+?)\]\]", r"\1", value)
+
+        d[key] = value
+
+print(d)
